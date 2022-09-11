fcd94e840dcd56449450d14782d1dda6674eadaf
Vinit Desai <desai.vinit@trilogy.com>
2022-09-11 01:20:47 -0400

    IC

--- src/col.py
+++ src/col.py
@@ -1,5 +1,5 @@
-from CSC510_43.src.sym import Sym
-from CSC510_43.src.num import Num
+from sym import Sym
+from num import Num
 
 class Cols(object):
     def __init__(self, names = []):
@@ -11,7 +11,7 @@ class Cols(object):
         for idx, name in enumerate(names):
             col = name[0].isupper() and Num(idx, name) or Sym(idx, name)
             self.all.append(col)
-            if not name[-1] == ':':
-                self.y.append(col) if name[-1] == '-' else self.x.append(col)
+            if name[-1] != ':':
+                self.y.append(col) if name[-1] == '-' or name[-1] == '+' else self.x.append(col)
                 if name[-1] == '!':
                     self.klass = col
---
+++ src/misc.py
@@ -0,0 +1,31 @@
+from main import the
+
+def isfloat(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+def coerce(string):
+    if isfloat(string):
+        return float(string)
+    elif string == "true":
+            return True
+    elif string == "false":
+        return False
+    else:
+        return string
+
+def csv(src, fn):
+    sep = the['seperator']
+    with open(src, "r") as f:
+        for line in f.readlines():
+            table = []
+            for value in line.split(sep):
+                table.append(coerce(value.strip()))
+            fn(table)
+
+def push(table, row):
+    table.append(row)
+    return row
