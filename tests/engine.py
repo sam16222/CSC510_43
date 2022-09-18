3713b2eeb208a67c2d1043a0804f5a99735388aa
Priyanka Ambawane <dearpriyankasa@gmail.com>
2022-09-18 17:47:08 -0400

    test commit: to open browser

--- tests/engine.py
+++ tests/engine.py
@@ -1,12 +1,14 @@
+from test_csv import test_csv
+from test_stats import test_stats
+from test_data import test_data
+from test_bignum import test_bignum
+from test_sym import test_sym
+from test_num import test_num
+from test_the import test_the
 import sys
 sys.path.insert(0, '../src')
-from test_the import test_the
-from test_num import test_num
-from test_sym import test_sym
-from test_bignum import test_bignum
-from test_data import test_data
-from test_stats import test_stats
-from test_csv import test_csv
+
+
 def main():
     result = 0
     result += test_the()
@@ -16,9 +18,11 @@ def main():
     result += test_data()
     result += test_stats()
     result += test_csv()
+    print(result, " temp changes")
     print(result, " tests failed")
     print(7 - result, " tests passed")
     return result
+
 
 if __name__ == '__main__':
     main()
